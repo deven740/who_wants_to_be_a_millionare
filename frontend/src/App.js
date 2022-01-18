4c84b9b4e5635457a876fffa55e94a89df4d4965
deven740 <devenparab740@gmail.com>
2022-01-18 11:59:24 +0530

    Adding the CSS and HTML for displaying the questions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,12 @@ import logo from "./homepage.png";
 import "./App.css";
 import Quiz from "./Quiz/Quiz";
 import MoneyLadder from "./MoneyLadder/MoneyLadder";
+import Game from "./Game/Game";
 
 function App() {
   return (
     <div className="app">
-      <div className="game">game</div>
+      <Game />
       <MoneyLadder />
     </div>
   );
---
+++ frontend/src/Game/Game.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Game.css";
+
+function Game() {
+  return (
+    <div className="game">
+      <div className="question">Who wants to be a millionare ?</div>
+      <br />
+      <div className="options">
+        <div className="option correct">A</div>
+        <div className="option wrong">B</div>
+        <div className="option">C</div>
+        <div className="option">D</div>
+      </div>
+    </div>
+  );
+}
+
+export default Game;
