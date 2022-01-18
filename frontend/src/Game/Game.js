c261648c1bc33f7feb0c18ded276b65802573bcc
deven740 <devenparab740@gmail.com>
2022-01-18 22:24:01 +0530

    Finished styling the CSS for questions

--- frontend/src/Game/Game.js
+++ frontend/src/Game/Game.js
@@ -7,8 +7,8 @@ function Game() {
       <div className="question">Who wants to be a millionare ?</div>
       <br />
       <div className="options">
-        <div className="option correct">A</div>
-        <div className="option wrong">B</div>
+        <div className="option">A</div>
+        <div className="option">B</div>
         <div className="option">C</div>
         <div className="option">D</div>
       </div>
